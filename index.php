5b951f5d7c30d7dae76e3f73ae4f2928608db05c
JuanA093 <andrade.juan0903@gmail.com>
2023-10-04 00:49:08 -0500

    Mediana, mediana & moda

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+    if($_SERVER ["REQUEST_METHOD"] == "POST"){
+        $dato1 = $_POST['dato1'];
+        $dato2 = $_POST['dato2'];
+        $dato3 = $_POST['dato3'];
+        $dato4 = $_POST['dato4'];
+    }
+
+    $datos = array($dato1, $dato2, $dato3, $dato4);
+
+    function calcularMedia($datos){
+
+        $total = array_sum($datos);
+        $media = $total / sizeof($datos);
+
+        return $media;
+
+    }
+
+    function calcularMediana($datos){
+
+        $mediana = $datos;
+        $longitudDatos = count($mediana);
+        $posicionMedia = floor($longitudDatos / 2);
+
+        if($longitudDatos % 2 != 0){
+
+            $valorMediana = $mediana[$posicionMedia];
+
+            return $valorMediana;
+
+        }else{
+
+            $posicionIzquierda = ($longitudDatos / 2 ) - 1;
+            $posicionDerecha = $longitudDatos / 2;
+            $izquierda = $mediana[$posicionIzquierda];
+            $derecha = $mediana[$posicionDerecha];
+
+            $valorMediana = ($izquierda + $derecha) / 2;
+
+            return $valorMediana;
+
+        }
+    }
+
+    function calcularModa($datos){
+
+        sort($datos);
+
+        $repeticiones = array();
+
+        foreach($datos as $i){
+            if(isset($repeticiones[$i])){
+                $repeticiones[$i]++;
+            }else{
+
+                $repeticiones[$i] = 1;
+
+            }
+        }
+
+        $moda = array_search(max($repeticiones), $repeticiones);
+
+        return $moda;
+
+    }
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="TablaResultados">
+        <table>
+            <tr>
+                <th>Media</th>
+                <th>Mediana</th>
+                <th>Moda</th>
+            </tr>
+            
+            <tr>
+                <td><?php echo calcularMedia($datos)?></td>
+                <td><?php echo calcularMediana($datos)?></td>
+                <td><?php echo calcularModa($datos)?></td>
+            </tr>
+        </table>
+
+
+    </section>
+    
+</body>
+</html>
